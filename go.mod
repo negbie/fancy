7f44366272dce6592e0634eff94d436f4d389590
negbie <nupagadi@posteo.de>
2019-10-21 20:21:50 +0200

    Add Go modules and some metrics

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/negbie/fancy
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/golang/snappy v0.0.1
+	github.com/peterbourgon/ff v1.6.0
+	github.com/prometheus/client_golang v1.2.1
+	github.com/prometheus/common v0.7.0
+	github.com/stretchr/testify v1.3.0
+	golang.org/x/sys v0.0.0-20191020212454-3e7259c5e7c2 // indirect
+)
--- main.go
+++ main.go
@@ -5,44 +5,71 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/peterbourgon/ff"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-const version = "1.0"
+const version = "1.1"
 
 func main() {
-	fmt.Fprintf(os.Stderr, "%v start fancy version %s\n", time.Now(), version)
+	fmt.Fprintf(os.Stderr, "%v start fancy v.%s with %s\n", time.Now(), version, os.Args[1:])
 	os.Exit(parseAndRun(os.Stderr, os.Stdin, os.Args[1:]))
 }
+
+var (
+	logScanNumber = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "fancy_input_scan_total",
+		Help: "Total number of logs received from rsyslog fancy template"},
+		[]string{"hostname", "program", "level", "facility"})
+	logHostProgSize = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "fancy_input_raw_bytes_total",
+		Help: "Total number of bytes received from rsyslog fancy template"},
+		[]string{"hostname", "program"})
+)
 
 func parseAndRun(stderr io.Writer, stdin io.Reader, args []string) int {
 	fs := flag.NewFlagSet("fancy", flag.ContinueOnError)
 	var (
 		lokiURL    = fs.String("lokiurl", "http://localhost:3100", "Loki Server URL")
-		chanSize   = fs.Int("chansize", 20000, "Loki buffered channel capacity")
-		batchSize  = fs.Int("batchsize", 600*1024, "Loki will batch some bytes before sending them")
+		chanSize   = fs.Int("chansize", 10000, "Loki buffered channel capacity")
+		batchSize  = fs.Int("batchsize", 100*1024, "Loki will batch some bytes before sending them")
 		batchWait  = fs.Int("batchwait", 4, "Loki will send logs after some seconds")
 		metricOnly = fs.Bool("metriconly", false, "Only metrics for prometheus will be exposed")
+		promAddr   = fs.String("promaddr", ":9090", "Metrics endpoint address")
 	)
 
+	lastWarn := time.Now()
 	err := ff.Parse(fs, args, ff.WithEnvVarPrefix("FANCY"))
 	if err != nil {
 		if err != flag.ErrHelp {
-			fs.Output().Write([]byte(fmt.Sprintf("\n%s\n", err)))
+			fmt.Fprintf(stderr, "%v ERROR: %v\n", lastWarn, err)
 		}
 		return 1
 	}
 
-	lastWarn := time.Now()
-	lineChan := make(chan *LogLine, *chanSize)
-	l, err := NewLoki(lineChan, *lokiURL, *batchSize, *batchWait)
-	if err != nil {
-		fmt.Fprintf(stderr, "%v ERROR: %v\n", lastWarn, err)
+	var lineChan chan *LogLine
+	if *metricOnly {
+		go func() {
+			http.Handle("/metrics", promhttp.Handler())
+			err := http.ListenAndServe(*promAddr, nil)
+			if err != nil {
+				fmt.Fprintf(stderr, "%v ERROR: %v\n", lastWarn, err)
+			}
+		}()
+	} else {
+		lineChan = make(chan *LogLine, *chanSize)
+		l, err := NewLoki(lineChan, *lokiURL, *batchSize, *batchWait)
+		if err != nil {
+			fmt.Fprintf(stderr, "%v ERROR: %v\n", lastWarn, err)
+		}
+		go l.Run()
 	}
-	go l.Run()
 
 	s := bufio.NewScanner(stdin)
 	for s.Scan() {
@@ -52,6 +79,9 @@ func parseAndRun(stderr io.Writer, stdin io.Reader, args []string) int {
 		}
 
 		if *metricOnly {
+			logSize := float64(len(ll.Raw))
+			logScanNumber.WithLabelValues(ll.Hostname, ll.Program, ll.Severity, ll.Facility).Inc()
+			logHostProgSize.WithLabelValues(ll.Hostname, ll.Program).Add(logSize)
 			continue
 		}
 
